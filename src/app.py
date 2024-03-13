758bcaf0e706e4fca00c4a3a7ff6edf71920d724
RicardoNarwhaI <ricardito.a.pastor@gmail.com>
2024-03-12 23:22:33 -0400

    stuff

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, jsonify
 import pandas as pd
-import numpy as np
-import pymc as pm
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,6 +8,9 @@ CORS(app)  # Allow CORS for all domains
 @app.route('/run-model', methods=['POST'])
 def run_model():
     try:
+        uploaded_file = request.files['file']
+        print(uploaded_file)  # Check if the file is received
+        
         # Extract data from the uploaded CSV file
         csv_file = request.files['file']
         df = pd.read_csv(csv_file)
